74bf854ce5aa7022c409084fd4cd4e4b2fb4d312
packo97 <pasqualegatto97@gmail.com>
2021-01-25 17:38:40 +0100

    added registration component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    RegistrationComponent,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
