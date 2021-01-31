c3c0e58343b360a7db2a6670bfd6eced93d54e3a
rodolfopietro97 <rodolfopietro97@gmail.com>
2021-01-31 16:51:24 +0100

    Create main structure of project

---
+++ blockchain/__init__.py
@@ -0,0 +1,8 @@
+"""Main package.
+
+It contains miner and client.
+A miner is the host that mine and validate transactions
+A client is a simple client that vote for a generic topic.
+
+All subpackages contains all useful dependencies
+"""
