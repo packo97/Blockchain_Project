79780dcc8aa6a86546f1ac67373837cbce0f7ad2
rodolfopietro97 <rodolfopietro97@gmail.com>
2021-02-08 10:50:10 +0100

    Added snippet code that spread to other miners

--- blockchain/comunication/grpc_comunication_handlers/TransactionServiceHandler.py
+++ blockchain/comunication/grpc_comunication_handlers/TransactionServiceHandler.py
@@ -6,8 +6,10 @@ from time import sleep
 import grpc
 from threading import Thread, RLock
 import asyncio
+import sys
 
 # Proto generated files
+from comunication.grpc_clients_handlers.TransactionHandlerClient import TransactionHandlerClient
 from comunication.grpc_protos import Transaction_pb2_grpc, Transaction_pb2
 from comunication.transactions.TransactionObject import TransactionObject
 from comunication.transactions.validation.TransactionValidator import TransactionValidator
@@ -55,11 +57,39 @@ class TransactionService(Transaction_pb2_grpc.TransactionServicer):
         # Already voter for event
         alreadyVoted = request.event in [transaction.event for transaction in self.miningStatus.receivedTransactions]
 
-        # Final
-        transactionValid = validSyntax and validSemantic and (not alreadySentToMiner) and (not alreadyVoted)
+        # # Send to other miner AT LEAST 3
+        # sentToAtLeast3Miners = False
+        #
+        # # Count the number of sending transactions
+        # sendingTransactionsCount = 0
+        #
+        # # Send transaction to miners (FOR EACH KNOWN HOST)
+        # for minerHostAddress in self.miningStatus.minerConfiguration.getKnownHosts():
+        #     try:
+        #         # Get response status (try to send transaction)
+        #         responseStatus = TransactionHandlerClient.sendTransaction(time=requestTransaction.time,
+        #                                                                   event=requestTransaction.event,
+        #                                                                   vote=requestTransaction.vote,
+        #                                                                   address=requestTransaction.address,
+        #                                                                   host=requestTransaction.minerHostAddress)
+        #
+        #         # Transaction is going well (no errors or other stuffs) we update sending transaction counter
+        #         if responseStatus:
+        #             sendingTransactionsCount = sendingTransactionsCount + 1
+        #
+        #     except Exception:
+        #         print(f"{minerHostAddress} is unreachable!", file=sys.stderr)
+        #
+        # # Verify if transaction is sented to AT LEAST 3 peers ("3 da chiazza protocol")
+        # if sendingTransactionsCount >= 3:
+        #     sentToAtLeast3Miners = True
 
-        # Append to transactions
+        # Final
+        transactionValid = validSyntax and validSemantic and (not alreadySentToMiner) and (not alreadyVoted) # and sentToAtLeast3Miners
+
+        # If transaction is valid we can append it to our transactions list
         if transactionValid:
+            # Append to transactions
             self.miningStatus.receivedTransactions.append(requestTransaction)
 
         # Return transaction with result
