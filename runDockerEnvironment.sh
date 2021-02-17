cf9d012d1e9adbd28748c63183f7cb6824c4ab4d
rodolfopietro97 <rodolfopietro97@gmail.com>
2021-02-17 18:45:47 +0100

    Removed buggy script

--- runDockerEnvironment.sh
+++
@@ -1,5 +0,0 @@
-gnome-terminal -- docker-compose up miner1 &\
-gnome-terminal -- docker-compose up miner2 &\
-gnome-terminal -- docker-compose up miner3 &\
-gnome-terminal -- docker-compose up miner4 &\
-gnome-terminal -- docker-compose up miner5
